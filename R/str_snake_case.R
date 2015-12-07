3c0482820925622c162de6d9f273d24b6b888d72
Christopher Brown <cbrown@decisionpatterns.com>
2015-12-07 10:48:36 -0800

    Version: 0.12.2
    - Fix str_snake_case
      - Multiple, adjacent _'s replaced by single _
      - Leading and trailing _'s dropped

--- R/str_snake_case.R
+++ R/str_snake_case.R
@@ -9,6 +9,8 @@
 #' * non- \code{\\w}, \code{\\s} and - are dropped
 #' * \code{\\w} and - are converted to underscore
 #' * no support for acronyms
+#' * multiple adjacent undescores are replaced by single underscore 
+#' * Underscores at beginning or end of names are dropped  
 #' 
 #' @examples
 #'   str_snake_case( "One Flew Over The Cuckoo's Nest" )
@@ -16,6 +18,7 @@
 #'   str_snake_case( "Catch.22" )
 #'   str_snake_case( "Catch_22" )
 #'   str_snake_case( "Catch  22" )
+#'   str_snake_case( " Catch 22 " )
 #'   
 #' @rdname str_snake_case
 #' @aliases str_snake_case
@@ -33,7 +36,11 @@ str_snake_case <- function(
   string <- gsub( '[^\\w\\s-\\.]', '', string, perl=TRUE )
   string <- tolower(string)
   string <- gsub( pattern_separators, '_', string, perl=TRUE )
+  string <- gsub( '__+', '_', string, perl=TRUE )  # Replace multiple with single _'s
+  string <- gsub( '^_+', '', string, perl=TRUE )   # Drop leading _'s if exist
+  string <- gsub( '_+$', '', string, perl=TRUE )   # Drop trailing _'s if exist
   
   return(string)
   
 }
+
--- tests/testthat/test-snake_case.r
+++ tests/testthat/test-snake_case.r
@@ -7,7 +7,8 @@ context( "str_snake_case" )
 str_snake_case( 'mission_of_burma' ) %>% expect_equal( 'mission_of_burma' ) 
 str_snake_case( 'mission-of-burma' ) %>% expect_equal( 'mission_of_burma' )
 str_snake_case( 'mission.of.burma' ) %>% expect_equal( 'mission_of_burma' )
-
+str_snake_case( 'Mission of Burma' ) %>% expect_equal( 'mission_of_burma' )
+str_snake_case( ' .mission.of.burma. ' ) %>% expect_equal( 'mission_of_burma' )
 
 # str_snake_case( 'mission-of-burma' ) %>% expect_equal( 'mission_of_burma' )
 # str_snake_case( 'mission_of_burma' ) %>% expect_equal( 'mission_of_burma' )
