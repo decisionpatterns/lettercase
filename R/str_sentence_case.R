f77a44b74a900465445fbfa1da0aae310b22a687
Guillaume Devailly <gdevailly@hotmail.com>
2017-12-12 13:54:30 +0100

    Update str_sentence_case.R
    removing bug

--- R/str_sentence_case.R
+++ R/str_sentence_case.R
@@ -12,6 +12,6 @@ str_sentence_case <- function( string ) {
     # adapted from https://stackoverflow.com/questions/18509527/first-letter-to-upper-case
     # behave as expected, even for short string (i.e. "" or "a")
     substr(string, 1, 1) <- toupper(substr(string, 1, 1))
-    substr(string, 2, nchar(string)) <- tolower(substr(string, 1, nchar(string)))
+    substr(string, 2, nchar(string)) <- tolower(substr(string, 2, nchar(string)))
     string
 }
