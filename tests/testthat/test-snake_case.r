86d0912174045014a098b2a759e7d3301fd8328d
Will Beasley <wibeasley@hotmail.com>
2017-12-21 10:02:26 -0600

    additional snake_case regression tests
    To make sure the CamelCase implementation didn't mess up other case conversions
    
    See #1

--- tests/testthat/test-snake_case.r
+++ tests/testthat/test-snake_case.r
@@ -12,5 +12,15 @@ str_snake_case( 'MissionOfBurma'   ) %>% expect_equal( 'mission_of_burma' )
 str_snake_case( 'missionOfBurma'   ) %>% expect_equal( 'mission_of_burma' )
 str_snake_case( ' .mission.of.burma. ' ) %>% expect_equal( 'mission_of_burma' )
 
-str_snake_case( 'mission'   ) %>% expect_equal( 'mission' )
-str_snake_case( 'Mission'   ) %>% expect_equal( 'mission' )
+str_snake_case( 'mission' )         %>% expect_equal( 'mission' )
+str_snake_case( 'Mission' )         %>% expect_equal( 'mission' )
+
+str_snake_case( "Catch-22" )        %>% expect_equal( 'catch_22' ) 
+str_snake_case( "Catch.22" )        %>% expect_equal( 'catch_22' )
+str_snake_case( "Catch_22" )        %>% expect_equal( 'catch_22' )
+str_snake_case( "Catch  22" )       %>% expect_equal( 'catch_22' )
+str_snake_case( " Catch 22 " )      %>% expect_equal( 'catch_22' )
+
+str_snake_case( "patient.dob" )     %>% expect_equal( 'patient_dob' )
+str_snake_case( "patientDob" )      %>% expect_equal( 'patient_dob' )
+str_snake_case( "PatientDob" )      %>% expect_equal( 'patient_dob' )
